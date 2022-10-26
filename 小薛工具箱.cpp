91f0c54beb48df776eccf590c4063d44594a95fc
CL科技团 CL <jou-ui@qq.com>
2022-10-26 11:12:59 +0800

    添加项目文件。

---
+++ 小薛工具箱.cpp
@@ -0,0 +1,78 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+int str_size;
+//菜单列表
+const char* str[] = { "关于我们","进入官网","更新工具箱","退出","扫描并修复win10系统文件[管理员]",
+"磁盘垃圾清理[系统自带]" };
+
+//输出菜单
+void list_data() {
+	system("color A");
+	system("title 小薛工具箱 - 小薛娱乐园出品 - V 1.0.0.1 ");
+	printf("\n功能列表：\n\n");
+	str_size = (sizeof str) / (sizeof str[0]);
+	for (int i = 0; i < str_size; i++) {
+
+		if (i % 5 != 0 || i == 0)
+		{
+			printf("[%d] %s   ", i, str[i]);
+		}
+		else {
+			printf("\n\n[%d] %s   ", i, str[i]);
+		}
+	}
+}
+
+//执行对应的功能
+void list_run() {
+restart:
+	list_data();
+	int max;
+	printf("\n\n请输入对应功能的序号：");
+	scanf_s("%d", &max);
+	if (str_size > max) {
+		switch (max)
+		{
+		case 0:
+			system("cls");
+			printf("\n关于我们：\n\n官网：http://www.52xcl.cn/\n\nQQ群：891198302\n\n微信公众号：小薛娱乐园\n\n");
+			system("pause");
+			system("cls");
+			goto restart;
+			break;
+		case 1:
+			system("cls && start http://www.52xcl.cn");
+			goto restart;
+			break;
+		case 2:
+			system("cls && curl -o update.exe http://api.52xcl.cn:88/toolbox/update.exe && start update.exe");
+			break;
+		case 3:
+			exit;
+			break;
+		case 4:
+			system("cls && sfc /scannow");
+			goto restart;
+			break;
+		case 5:
+			system("cls && cleanmgr");
+			goto restart;
+			break;
+
+		}
+	}
+	else
+	{
+		system("cls");
+		printf("\n请输入[%d]到[%d]内的序列号！\n", 0, str_size - 1);
+		goto restart;
+	}
+}
+//主程序
+int main() {
+	list_run();
+	return 0;
+}
